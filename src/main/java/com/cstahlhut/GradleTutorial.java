f9b690513ec0a8a1807d060f2bfa7beb049e7c8d
Stollie1980 <cheynestahlhut@gmail.com>
2022-02-26 23:27:36 +1000

    initial build

---
+++ src/main/java/com/cstahlhut/GradleTutorial.java
@@ -0,0 +1,7 @@
+package com.cstahlhut;
+
+public class GradleTutorial {
+    public static void main(String[] args) {
+        System.out.println("Gradle 4tw!");
+    }
+}
---
+++ src/test/java/com/cstahlhut/GradleTutorialTest.java
@@ -0,0 +1,13 @@
+package com.cstahlhut;
+
+import java.beans.Transient;
+
+import org.junit.Test;
+
+public class GradleTutorialTest {
+
+    @Test
+    public void verifyNoExceptionThrown() {
+        GradleTutorial.main(new String[]{});
+    }
+}
